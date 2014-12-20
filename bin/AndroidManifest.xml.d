dd2bfd72fa1c01f2129b92931d389d0dbfb4f1d9
TeamDevMat <teamdev.mat@gmail.com>
2014-12-20 17:43:42 -0600

    Iniciando

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/issac/workspace/Examples/BusPoint/bin/AndroidManifest.xml : \
+/home/issac/workspace/Examples/BusPoint/AndroidManifest.xml \
---
+++ bin/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+/home/issac/workspace/Examples/BusPoint/bin/MainActivity-debug-unaligned.apk : \
+/home/issac/workspace/Examples/BusPoint/bin/MainActivity.ap_ \
+/home/issac/workspace/Examples/BusPoint/bin/classes.dex \
+/home/issac/workspace/appcompat_v7/bin/classes.jar \
+/home/issac/workspace/appcompat_v7/libs/android-support-v7-appcompat.jar \
+/home/issac/workspace/appcompat_v7/libs/android-support-v4.jar \
