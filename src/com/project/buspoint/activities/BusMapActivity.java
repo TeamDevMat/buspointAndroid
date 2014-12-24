dda127319e2b19c0afb262c5d5a67cbb5de31062
TeamDevMat <teamdev.mat@gmail.com>
2014-12-23 21:30:14 -0600

    Activity que usa el mapa para mostrar rutas, peseras

---
+++ src/com/project/buspoint/activities/BusMapActivity.java
@@ -0,0 +1,12 @@
+package com.project.buspoint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BusMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+}
