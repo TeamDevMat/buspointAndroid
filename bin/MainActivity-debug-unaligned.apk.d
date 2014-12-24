931e9513ea84e2956d38b22b49dffc655e61a2de
TeamDevMat <teamdev.mat@gmail.com>
2014-12-23 22:08:49 -0600

    Update bin's

--- bin/MainActivity-debug-unaligned.apk.d
+++ bin/MainActivity-debug-unaligned.apk.d
@@ -2,5 +2,7 @@
 /home/issac/workspace/Examples/BusPoint/bin/MainActivity.ap_ \
 /home/issac/workspace/Examples/BusPoint/bin/classes.dex \
 /home/issac/workspace/Examples/BusPoint/appcompat_v7/bin/classes.jar \
+/home/issac/workspace/Examples/BusPoint/google-play-services_lib/bin/classes.jar \
 /home/issac/workspace/Examples/BusPoint/appcompat_v7/libs/android-support-v7-appcompat.jar \
+/home/issac/workspace/Examples/BusPoint/google-play-services_lib/libs/google-play-services.jar \
 /home/issac/workspace/Examples/BusPoint/appcompat_v7/libs/android-support-v4.jar \
