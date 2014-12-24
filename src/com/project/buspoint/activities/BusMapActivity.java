16f5cf34b225bc73766f8d76c4a31ea3d3ea3e53
TeamDevMat <teamdev.mat@gmail.com>
2014-12-23 22:04:35 -0600

    Corrigio: Referencia a R.class, package incompleto, layout equivocado

--- src/com/project/buspoint/activities/BusMapActivity.java
+++ src/com/project/buspoint/activities/BusMapActivity.java
@@ -1,13 +1,15 @@
-package com.project.buspoint;
+package com.project.buspoint.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import  com.project.buspoint.R;
 
 public class BusMapActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.map.xml);
     }
 }
