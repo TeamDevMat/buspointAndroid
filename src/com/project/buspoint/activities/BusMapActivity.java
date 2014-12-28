1a72a293ff23837538d48b8e087ee1e795596980
TeamDevMat <teamdev.mat@gmail.com>
2014-12-28 14:55:17 -0600

    Obtener la localizacion actual

--- src/com/project/buspoint/activities/BusMapActivity.java
+++ src/com/project/buspoint/activities/BusMapActivity.java
@@ -2,14 +2,53 @@ package com.project.buspoint.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationServices;
 
 import  com.project.buspoint.R;
 
-public class BusMapActivity extends Activity {
+public class BusMapActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener{
 
+
+    public GoogleApiClient mGoogleClient;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+   	buildGoogleApiClient();
+	
     }
+
+
+    protected synchronized void buildGoogleApiClient(){
+	mGoogleClient = new GoogleApiClient.Builder(this)
+			.addConnectionCallbacks(this)
+			.addOnConnectionFailedListener(this)
+			.addApi(LocationServices.API)
+			.build();
+
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+	Toast.makeText(this, "Connected to location services API",Toast.LENGTH_SHORT).show();	
+   }
+
+
+   @Override
+   public void onDisconnected(){
+	//nothing	
+   }
+
+   @Override
+   public void onConnectionFailed(ConnectionResult arg0){
+	
+	
+   } 			
 }
