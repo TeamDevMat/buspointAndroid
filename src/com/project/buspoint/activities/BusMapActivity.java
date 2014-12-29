4ca333e3a2ede4e582f0086fc190d292aed17b9c
TeamDevMat <teamdev.mat@gmail.com>
2014-12-28 18:51:29 -0600

    El orden importa en setContentView y obtener el mapa?

--- src/com/project/buspoint/activities/BusMapActivity.java
+++ src/com/project/buspoint/activities/BusMapActivity.java
@@ -3,11 +3,18 @@ package com.project.buspoint.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.location.Location;
+
+
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 
 import  com.project.buspoint.R;
 
@@ -15,36 +22,61 @@ public class BusMapActivity extends Activity implements ConnectionCallbacks, OnC
 
 
     public GoogleApiClient mGoogleClient;
+    public Location mLastLocation;
+    public GoogleMap mMap;	    
     
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map);
-   	buildGoogleApiClient();
+        super.onCreate(savedInstanceState);	
+	setContentView(R.layout.map);
+
+	mMap =  ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+	if(mMap == null)
+		return;	
+
+	buildGoogleApiClient();
+	
+   	
 	
     }
 
+    @Override
+    protected void onStart(){
+	super.onStart();
+	mGoogleClient.connect();
 
+    }	
     protected synchronized void buildGoogleApiClient(){
 	mGoogleClient = new GoogleApiClient.Builder(this)
 			.addConnectionCallbacks(this)
 			.addOnConnectionFailedListener(this)
 			.addApi(LocationServices.API)
 			.build();
-
+	
     }
+	
 
+    
     @Override
     public void onConnected(Bundle connectionHint){
-	Toast.makeText(this, "Connected to location services API",Toast.LENGTH_SHORT).show();	
-   }
+
+	mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleClient);
+	Toast.makeText(this,"Lat:" + mLastLocation.getLatitude() + "Lng:" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+   	
+	//mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng( mLastLocation.getLatitude(), mLastLocation.getLongitude()),10));
+
+	}
 
 
+    @Override
+    protected void onStop(){
+	super.onStop();
+	if(mGoogleClient.isConnected())
+		mGoogleClient.disconnect();	
+    }	
    @Override
-   public void onDisconnected(){
-	//nothing	
-   }
+   public void onConnectionSuspended(int cause){}	
 
    @Override
    public void onConnectionFailed(ConnectionResult arg0){
