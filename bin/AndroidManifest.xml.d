76e5dabba3a17a3e564f2157b75d6c514c339964
TeamDevMat <teamdev.mat@gmail.com>
2015-01-16 23:07:44 -0600

    Super cambio

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/issac/workspace/Examples/BusPoint/bin/AndroidManifest.xml : \
-/home/issac/workspace/Examples/BusPoint/AndroidManifest.xml \
--- bin/MainActivity-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/home/issac/workspace/Examples/BusPoint/bin/MainActivity-debug-unaligned.apk : \
-/home/issac/workspace/Examples/BusPoint/bin/MainActivity.ap_ \
-/home/issac/workspace/Examples/BusPoint/bin/classes.dex \
-/home/issac/workspace/Examples/BusPoint/google-play-services_lib/bin/classes.jar \
-/home/issac/workspace/Examples/BusPoint/libs/android-support-v7-appcompat.jar \
-/home/issac/workspace/Examples/BusPoint/libs/android-support-v4.jar \
-/home/issac/workspace/Examples/BusPoint/google-play-services_lib/libs/google-play-services.jar \
